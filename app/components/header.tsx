32f87f2e62cd8fe99e51a763f69e887d147a12f8
Jasper Chan <jasperchan@gmail.com>
2024-10-28 17:44:49 -0700

    Update header component to replace Mail icon with SendHorizontal and add Gmail and Outlook logos

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,12 +1,35 @@
-import { Mail } from "lucide-react";
+import { SendHorizonal } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 export function Header() {
   return (
     <div className="text-center">
       <div className="flex justify-center mb-6">
-        <Link href="/">
-          <Mail className="h-12 w-12 text-blue-600" />
+        <Link href="/" className="flex items-center relative gap-2">
+          <Image
+            className="inline-flex items-center"
+            src="/gmail.png"
+            alt="Gmail Logo"
+            width={64}
+            height={48}
+          />
+          <div className="inline-block w-4 border-t-4 border-dotted border-gray-300" />
+          <SendHorizonal
+            className="stroke-gray-300"
+            height={32}
+            width={32}
+            strokeWidth={2}
+            absoluteStrokeWidth={true}
+          />
+          <div className="inline-block w-4 border-t-4 border-dotted border-gray-300" />
+          <Image
+            className="inline-flex items-center"
+            src="/outlook.webp"
+            alt="Outlook Logo"
+            width={64}
+            height={64}
+          />
         </Link>
       </div>
       <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl mb-6">
